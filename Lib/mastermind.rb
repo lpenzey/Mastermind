c217d658e19c967738913f0a9d094c998625ce8b
lpenzey <lucaspenzeymoog@gmail.com>
2018-06-20 14:53:33 -0500

    added player directions, secret code generator, getting user input

--- Lib/mastermind.rb
+++ Lib/mastermind.rb
@@ -1,52 +1,59 @@
-
-
-
 class Mastermind
+  def show_instructions
+    puts 'Welcome to Mastermind! Your challenge is
+to guess the secret four digit code in under 10 tries.
 
-	attr_reader :colors
-
-	def initialize
-		@colors = ['r', 'g', 'o', 'b', 'y', 'p']
-	end	
-
-	def show_instructions	
-		puts 'Welcome to Mastermind! Your challenge is to guess the secret 
-four digit code in under 10 tries.
 The code can include the following colors:
 Red(r), Green(g), Orange(o), Blue(b), Yellow(y) and Purple(p)
-Enter your guess by entering four color values from above, such as rgvb
+Enter your guess by entering four color values from above, such as rgyb or pggy.
+
 If you have a correct color in a correct position, you get a white pin.
 If you have a correct color in a different position, you get a black pin
 press \'q\' to quit, or enter your first guess.'
-	
-	end
+  end
 end
 
 class Code
+  attr_reader :code
 
-	def generate_code
-		code = []
-		4.times do |inject| 
-			code << @colors.sample
-		end
-		code
-	end
-end	
-
-class Response
-
-	def ask_for_input
-		print "Please enter a guess in the following format: ****\n"
-		print "Remember, the avialble colors are Red(r), Green(g), Orange(o), Blue(b), Yellow(y) and Purple(p)"
-	end	
-
-	def receive_valid_input
-		input = gets.chomp
-		if input != :r
-			puts "Invalid response, please "
-		end
-	end
-
+  def generate_code
+    code = []
+    @colors = ["r", "g", "o", "b", "y", "p"]
+    4.times do |inject|
+      code << @colors.sample
+    end
+    code
+  end
 end
 
+class Response
+  attr_reader :player_input
 
+  def initialize
+    @valid_guess = false
+    @player_input = ""
+  end
+
+  def get_input
+    puts "Please enter a guess from the available colors: r, g, o, b, y, p"
+    @player_input = gets.chomp
+    return @player_input.downcase.gsub(/[\W]/, "")
+  end
+
+  def validate_length(guess)
+    if @player_input.length != 4
+      puts "Invalid response, please enter exactly 4 colors"
+    end
+  end
+end
+
+def main
+  game = Mastermind.new
+  game.show_instructions
+  secret_code = Code.new.generate_code
+  input = Response.new.get_input
+end
+
+if __FILE__ == $0
+  main
+end
--- Spec/mastermind_spec.rb
+++ Spec/mastermind_spec.rb
@@ -1,35 +1,33 @@
-require 'mastermind'
+require "mastermind"
 
 RSpec.describe Mastermind, "welcome user and show game commands" do
   mastermind = Mastermind.new
- 
+
   it "greets the player with game instructions " do
     expect { mastermind.show_instructions }.to output(/Welcome/).to_stdout
   end
-end 
+end
 
 RSpec.describe Code, "creates a code" do
   code = Code.new
- 
-let(:colors)  { ['r', 'g', 'o', 'b', 'y', 'p'] }
+
+  let(:colors) { ["r", "g", "o", "b", "y", "p"] }
 
   it "generates a 4 digit code" do
     expect(code.generate_code.size).to eq 4
   end
 
-   it "only generates code from available colors" do
-    expect(code.generate_code).to include colors.sample(4)	
+  it "only uess values from available colors" do
+    code.generate_code.each do |char|
+      expect(["r", "g", "o", "b", "y", "p"].include?(char)).to eq(true)
+    end
   end
 end
 
 RSpec.describe Response, "receive valid input" do
   response = Response.new
- 
-  it "prompts user to enter a guess" do
-    expect { response.ask_for_input }.to output("Please enter a guess in the following format: ****\nRemember, the avialble colors are Red(r), Green(g), Orange(o), Blue(b), Yellow(y) and Purple(p)").to_stdout	
-  end
 
-  it "validates input" do
-    expect { response.receive_valid_input }.to eq __	
+  it "ensures there are only 4 letters entered" do
+    expect { response.validate_length("abcf") }.to output("Invalid response, please enter exactly 4 colors\n").to_stdout
   end
 end
