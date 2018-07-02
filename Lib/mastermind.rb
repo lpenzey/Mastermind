c72c484b1a8d9940353f047c1fccd6f00a9c536c
lpenzey <lucaspenzeymoog@gmail.com>
2018-07-02 14:10:37 -0500

    added game loop, code comparison logic, win condition

--- Lib/mastermind.rb
+++ Lib/mastermind.rb
@@ -1,24 +1,97 @@
-class Mastermind
+
+class Welcome
   def show_instructions
-    puts 'Welcome to Mastermind! Your challenge is
-to guess the secret four digit code in under 10 tries.
-
-The code can include the following colors:
+    puts "Welcome to Mastermind! Your challenge is
+to guess the secret four digit code in under 10 tries."
+    #sleep 5
+    puts ""
+    puts "The code can include the following colors:
 Red(r), Green(g), Orange(o), Blue(b), Yellow(y) and Purple(p)
-Enter your guess by entering four color values from above, such as rgyb or pggy.
+Enter your guess by entering four color values from above, such as rgyb or pggy."
+    #sleep 7
+    puts ""
+    puts "If you have a correct color in the right position, you get a red pin.
+If you have a correct color in the wrong position, you get a white pin"
+    #sleep 5
+    puts ""
+    puts 'press \'q\' to quit, or hit enter to continue.'
+    quit if gets.chomp.downcase == "q"
+  end
 
-If you have a correct color in a correct position, you get a white pin.
-If you have a correct color in a different position, you get a black pin
-press \'q\' to quit, or enter your first guess.'
+  def quit
+    abort("It's been an honor serving with you. Farewell!")
   end
 end
 
 class Code
   attr_reader :answer
+  attr_reader :player_input
+  attr_reader :red_pins
+  attr_reader :white_pins
+  attr_reader :position
+  attr_reader :secret
+
 
   def generate_code
     @colors = colors = ["r", "g", "o", "y", "b", "p"]
-    answer = (1..4).map { @colors[rand(6)] }
+    @answer = (1..4).map { @colors[rand(6)] }
+  end
+
+  def red_counter(key, input)
+    red_pins = []
+    position = 0
+    while input.length > position
+      key.each do |match|
+        red_pins << 1 if key[position] == input[position]
+        position += 1
+      end
+    end
+    red_pins
+  end
+
+  def trim(key, input)
+    position = 0
+    if input[position] == key[position]
+      input[position] = ""
+      key[position] = ""
+    end
+    position += 1
+    if input[position] == key[position]
+      input[position] = ""
+      key[position] = ""
+    end
+    position += 1
+    if input[position] == key[position]
+      input[position] = ""
+      key[position] = ""
+    end
+    position += 1
+    if input[position] == key[position]
+      input[position] = ""
+      key[position] = ""
+    end
+    position += 1
+    input
+  end
+
+  def white_counter(key, input)
+    x = (key & input).flat_map { |n| [n]*[key.count(n), input.count(n)].min }
+    white_pins = x.reject { |c| c.empty? }
+
+  end
+
+  def feedback(white_pins, red_pins)
+    puts "You have " + red_pins.length.to_s + " red pins and " + white_pins.compact.length.to_s + " white pins. Keep going!"
+  end
+
+  def won
+    puts "Congratulations, you've cracked the code!"
+    puts "Press \"r\" to restart or anything else to quit"
+    if gets.chomp.downcase == "r"
+      load 'mastermind.rb' 
+    else 
+      abort("Thanks for playing!")
+    end
   end
 end
 
@@ -31,10 +104,10 @@ class Response
   end
 
   def get_input
-    puts "Please enter a guess from available colors: r, g, o, b, y, p"
+    puts "Please enter a guess [****]"
     @player_input = gets.chomp.downcase.gsub(/[\W]/, "")
     get_input if length_error || color_error
-    @player_input
+    @player_input.split(//)
   end
 
   def valid_length?
@@ -59,10 +132,19 @@ class Response
 end
 
 def main
-  game = Mastermind.new
-  game.show_instructions
-  answer = Code.new.generate_code
-  input = Response.new(input).get_input
+  game = Welcome.new.show_instructions
+  code1 = Code.new
+  secret = code1.generate_code
+  10.times do
+    key = secret.clone
+    input = Response.new(input).get_input
+    red_pins = code1.red_counter(key, input)
+    code1.won if red_pins == [1, 1, 1, 1]
+    code1.trim(key, input)
+    white_pins = code1.white_counter(key, input)
+    code1.feedback(white_pins, red_pins)
+  end
+  print "Nice try, but the answer was " + secret.to_s + " better luck next time!"
 end
 
 if __FILE__ == $0
--- Spec/mastermind_spec.rb
+++ Spec/mastermind_spec.rb
@@ -1,7 +1,7 @@
-require "mastermind"
+require "mastermind" # ~> LoadError: cannot load such file -- mastermind
 
-RSpec.describe Mastermind, "welcome user and show game commands" do
-  let(:mastermind) { Mastermind.new }
+RSpec.describe Welcome, "welcome user and show game commands" do
+  let(:mastermind) { Welcome.new }
 
   it "greets the player with game instructions " do
     expect { mastermind.show_instructions }.to output(/Welcome/).to_stdout
@@ -20,6 +20,100 @@ RSpec.describe Code, "creates a code" do
     code.generate_code.each do |char|
       expect(["r", "g", "o", "b", "y", "p"].include?(char)).to eq(true)
     end
+  end
+end
+
+RSpec.describe Code, "compares guess with secret_key" do
+  let(:code) { Code.new }
+  let(:colors) { ["r", "g", "o", "y", "b", "p"] }
+  let(:key) { ["r", "r", "r", "r"] }
+  let(:input) { ["r", "r", "r", "r"] }
+
+  it "returns number of red pins" do
+    expect(code.red_counter(key, input)).to eq [1, 1, 1, 1]
+  end
+end
+
+RSpec.describe Code, "compares guess with secret_key" do
+  let(:code) { Code.new }
+  let(:colors) { ["r", "g", "o", "y", "b", "p"] }
+  let(:key) { ["r", "r", "y", "b"] }
+  let(:input) { ["r", "r", "p", "o"] }
+
+  it "returns number of red pins" do
+    expect(code.red_counter(key, input)).to eq [1, 1]
+  end
+end
+
+RSpec.describe Code, "trims code" do
+  let(:code) { Code.new }
+  let(:key) { ["r", "g", "y", "r"] }
+  let(:input) { ["r", "o", "b", "r"] }
+
+  it "deletes input position if it is the same as key index" do
+    expect(code.trim(key, input)).to eq ["", "o", "b", ""]
+  end
+end
+
+RSpec.describe Code, "trims code" do
+  let(:code) { Code.new }
+  let(:key) { ["y", "b", "p", "o"] }
+  let(:input) { ["y", "b", "p", "r"] }
+
+  it "deletes input position if it is the same as key index" do
+    expect(code.trim(key, input)).to eq ["", "", "", "r"]
+  end
+end
+
+RSpec.describe Code, "counts white pins" do
+  let(:code) { Code.new }
+  let(:key) { ["y", "b", "y", "r"] }
+  let(:input) { ["", "y", "b", ""] }
+
+  it "accurately counts white pins" do
+    expect(code.white_counter(key, input)).to eq ["y", "b"]
+  end
+end
+
+RSpec.describe Code, "counts white pins" do
+  let(:code) { Code.new }
+  let(:key) { ["b", "y", "y", "b"] }
+  let(:input) { ["b", "", "", ""] }
+
+  it "accurately counts white pins with duplicates" do
+    expect(code.white_counter(key, input)).to eq ["b"]
+  end
+end
+
+RSpec.describe Code, "counts white pins" do
+  let(:code) { Code.new }
+  let(:key) { ["g", "g", "r", "r"] }
+  let(:input) { ["r", "r", "g", "g"] }
+
+  it "accurately counts white pins with duplicates" do
+    expect(code.white_counter(key, input)).to eq ["g", "g", "r", "r"]
+  end
+end
+
+RSpec.describe Code, "counts white pins" do
+  let(:code) { Code.new }
+  let(:key) { ["b", "g", "r", "g"] }
+  let(:input) { ["r", "", "b", ""] }
+
+  it "accurately counts white pins with duplicates" do
+    expect(code.white_counter(key, input)).to eq ["b", "r"]
+  end
+end
+
+RSpec.describe Code, "feedback" do
+  let(:code) { Code.new }
+  let(:key) { ["y", "b", "p", "o"] }
+  let(:input) { ["y", "b", "p", "r"] }
+  let(:white_pins) { [1, 1] }
+  let(:red_pins) { [1, 1] }
+
+  it "gives pin feedback to user" do
+    expect { code.feedback(white_pins, red_pins)}.to output("You have " + red_pins.length.to_s + " red pins and " + white_pins.length.to_s + " white pins. Keep going!").to_stdout
   end
 end
 
@@ -46,3 +140,10 @@ RSpec.describe Response, "validate input colors" do
     expect { response.color_error }.to output(/Invalid colors/).to_stdout
   end
 end
+
+# ~> LoadError
+# ~> cannot load such file -- mastermind
+# ~>
+# ~> /Users/lucaspenzeymoog/.rbenv/versions/2.3.1/lib/ruby/2.3.0/rubygems/core_ext/kernel_require.rb:55:in `require'
+# ~> /Users/lucaspenzeymoog/.rbenv/versions/2.3.1/lib/ruby/2.3.0/rubygems/core_ext/kernel_require.rb:55:in `require'
+# ~> /Users/lucaspenzeymoog/Desktop/Coding/Mastermind/Spec/mastermind_spec.rb:1:in `<main>'
