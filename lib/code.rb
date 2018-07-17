bfc471428645438227ef1d5f7332f52f5cc70d3c
lpenzey <lucaspenzeymoog@gmail.com>
2018-07-17 15:01:56 -0500

    game loop captures previous guesses

--- lib/code.rb
+++ lib/code.rb
@@ -6,6 +6,7 @@ class Code
   
   CodeLength = 1..4
   ColorBank = 6
+  
   def generate_code
     @colors = colors = ["r", "g", "o", "y", "b", "p"]
     @answer = (CodeLength).map { @colors[rand(ColorBank)] }
